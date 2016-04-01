6e2e962afacb599eb0b22bc697dd9ab73142d67e
wookiepeter <matthis.hagen@o2online.de>
2016-04-01 22:23:33 +0200

    Hud hinzugefügt, MousePos relativ zum Fenster anzeigen, Schriftarten hinzugügt

--- GameJam/GameJam/Game.cs
+++ GameJam/GameJam/Game.cs
@@ -12,8 +12,11 @@ namespace GameJam
     class Game
     {
         RenderWindow window;
+        Hud hud;
+
         public Game()
         {
+            hud = new Hud();
             window = new RenderWindow(new VideoMode(800, 600), "GameJam");
             window.Closed += (object sender, EventArgs e) => { (sender as Window).Close(); };
             MapGenerator map = new MapGenerator(500);
@@ -21,6 +24,7 @@ namespace GameJam
             while (window.IsOpen)
             {
                 window.Clear(Color.Blue);
+                hud.DrawHud(window);
 
                 window.Display();
                 window.DispatchEvents();
---
+++ GameJam/GameJam/Hud.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace GameJam
+{
+    class Hud
+    {
+        private Font standardFont;
+        private Text exampleText;
+        private List<HudText> textList;
+        private Vector2i mousePosition;
+
+        public Hud()
+        {
+            
+            textList = new List<HudText>();
+            exampleText = new Text();
+            standardFont = new Font("fonts/arial.ttf");
+            textList.Add(new HudText("mousePos", "---", new Vector2f(0, 0)));
+        }
+        
+        void UpdateHud(RenderWindow window)
+        {
+            mousePosition = Mouse.GetPosition(window);
+            // searches HudText mousPos and udpates the output
+            textList.Find(i => i.textName.Equals("mousePos")).ChangeText("MousePos: X = " + mousePosition.X + "; Y = " + mousePosition.Y);
+        }
+
+        public void DrawHud(RenderWindow window)
+        {
+            UpdateHud(window);
+            foreach (HudText text in textList)
+            {
+                window.Draw(text.getText());
+            }
+        }
+    }
+}
