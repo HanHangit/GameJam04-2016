ce6ff6a9b33ffe4161ebe9d7e96bd297b350f85a
HanHangit <clonkcool@yahoo.de>
2016-04-01 19:09:53 +0200

    Es geht los

---
+++ GameJam/GameJam/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace GameJam
+{
+    class Game
+    {
+        RenderWindow window;
+        public Game()
+        {
+            window = new RenderWindow(new VideoMode(800, 600), "GameJam");
+            window.Closed += (object sender, EventArgs e) => { (sender as Window).Close(); };
+
+            while (window.IsOpen)
+            {
+                window.Clear(Color.Blue);
+
+                window.Display();
+                window.DispatchEvents();
+            }
+        }
+    }
+}
