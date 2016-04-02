a03fb6ba5f8cefd4c035354e45e2d16289443a1d
wookiepeter <matthis.hagen@o2online.de>
2016-04-02 14:26:29 +0200

    Klasse und Konstruktoren für Siedlungen erstellt

--- GameJam/GameJam/Hud.cs
+++ GameJam/GameJam/Hud.cs
@@ -15,6 +15,7 @@ namespace GameJam
         private Font standardFont;
 
         private List<HudText> textList;
+        private List<Settlement> settleList;
 
         private Vector2i mousePosition;
         private TimeSpan totalTime;
@@ -24,14 +25,16 @@ namespace GameJam
         public Hud()
         {
             textList = new List<HudText>();
+            settleList = new List<Settlement>();
             standardFont = new Font("fonts/arial.ttf");
             exampleSprite = new Sprite();
-            //RIGHTHERE CONTINUE
+            
             exampleSprite.Texture = new Texture(new Image("textures/Small_Village_Center.png"));
             
 
             textList.Add(new HudText("mousePos", "---", new Vector2f(0, 0)));
             textList.Add(new HudText("totalTime", "---", new Vector2f(0, 20)));
+
         }
        
         public void Update(RenderWindow window, GameTime gameTime)
@@ -39,7 +42,14 @@ namespace GameJam
             mousePosition = Mouse.GetPosition(window);
             totalTime = gameTime.TotalTime;
             stringTime = totalTime.Minutes + ":" + totalTime.Seconds.ToString("00.") + ":" + totalTime.Milliseconds;
-            exampleSprite.Position = new Vector2f(mousePosition.X, mousePosition.Y);
+            
+            exampleSprite.Position = new Vector2f(mousePosition.X - exampleSprite.TextureRect.Width, mousePosition.Y - exampleSprite.TextureRect.Height);
+
+            if (Mouse.IsButtonPressed(Mouse.Button.Right))
+            {
+                settleList.Add(new Settlement(new Sprite(new Texture("textures/Small_Village_Center.png")), mousePosition));
+
+            }
 
             // searches HudText mousPos and udpates the output
             textList.Find(i => i.textName.Equals("mousePos")).ChangeText("MousePos: X = " + mousePosition.X + "; Y = " + mousePosition.Y);
@@ -52,6 +62,12 @@ namespace GameJam
         public void DrawHud(RenderWindow window)
         {
             window.Draw(exampleSprite);
+            foreach( Settlement city in settleList)
+            {
+                Console.WriteLine(city.getSprite().Position.ToString());
+                window.Draw(city.getSprite());
+            }
+
             foreach (HudText text in textList)
             {
                 window.Draw(text.getText());
---
+++ GameJam/GameJam/Settlement.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace GameJam
+{
+    class Settlement
+    {
+        // Overall Variables
+        Vector2i realPosition; // realPosition = centered on the Sprite
+        Sprite sprite;
+
+
+        // Economy Variables;
+
+        public Settlement(Vector2i mousePosition)
+        {
+            Sprite defaultSprite = new Sprite(new Texture("textures/Small_Village_Center.png"));
+            sprite = defaultSprite;
+            sprite.Position = new Vector2f(mousePosition.X - sprite.TextureRect.Width, mousePosition.Y - sprite.TextureRect.Height);
+            realPosition = new Vector2i(mousePosition.X - sprite.TextureRect.Width / 2, mousePosition.Y - sprite.TextureRect.Height / 2);
+        }
+
+        public Settlement(Sprite _sprite, Vector2i mousePosition)
+        {
+            sprite = _sprite;
+            sprite.Position = new Vector2f(mousePosition.X - sprite.TextureRect.Width, mousePosition.Y - sprite.TextureRect.Height);
+            realPosition = new Vector2i(mousePosition.X - sprite.TextureRect.Width / 2, mousePosition.Y - sprite.TextureRect.Height / 2);
+        }
+
+        /// <summary>
+        /// this constructor is only for creating Objects manually
+        /// </summary>
+        /// <param name="_sprite"></param>
+        /// <param name="_realPosition">this is only because i wanted a different constructor</param>
+        public Settlement(Sprite _sprite, Vector2f _realPosition)
+        {
+            sprite = _sprite;
+            realPosition = new Vector2i((int)_realPosition.X, (int)_realPosition.Y);
+        }
+     
+        public Sprite getSprite()
+        {
+            return sprite;
+        }
+        
+
+    }
+}
